9a4c6bd717aa8717a4543be1b9ce255689e293d0
Apostolos Karamanis <apostkaram@gmail.com>
2025-03-11 00:53:53 +0200

    minor changes

---
+++ components/header/header-link.tsx
@@ -0,0 +1,49 @@
+import { motion } from "motion/react";
+import Link from "next/link";
+
+export default function HeaderLink({
+  text,
+  href,
+  isActive,
+  isHovered,
+  setIsHovered,
+  index,
+}: {
+  text: string;
+  href: string;
+  isActive: boolean;
+  isHovered: number | null;
+  setIsHovered: (isHovered: number | null) => void;
+  index: number;
+}) {
+  return (
+    <Link
+      href={href}
+      onMouseEnter={() => setIsHovered(index)}
+      onMouseLeave={() => setIsHovered(null)}
+      className="text-white relative tracking-tight text-3xl px-4 py-2 flex items-center justify-center"
+    >
+      <span className="z-10 -mt-2">{text}</span>{" "}
+      {isActive && isHovered === null && (
+        <motion.span
+          className="absolute inset-0 bg-[#9bd2ff]/15 rounded-full z-0"
+          layoutId="header-link-underline"
+          transition={{
+            duration: 0.8,
+            ease: [0.175, 0.885, 0.32, 1],
+          }}
+        />
+      )}
+      {isHovered === index && (
+        <motion.span
+          className="absolute inset-0 bg-[#9bd2ff]/15 rounded-full z-0"
+          layoutId="header-link-underline"
+          transition={{
+            duration: 0.8,
+            ease: [0.175, 0.885, 0.32, 1],
+          }}
+        />
+      )}
+    </Link>
+  );
+}
--- components/header/site-header.tsx
+++ components/header/site-header.tsx
@@ -8,7 +8,7 @@ export function SiteHeader() {
   return (
     <header className="fixed top-8 z-50 bg-jjBlack rounded-full whitespace-nowrap w-fit left-1/2 -translate-x-1/2 flex justify-between items-center p-2.5">
       <Link href="/" className="flex items-center ml-2 mr-16">
-        <span className="block text-2xl text-white">Repower Solutions</span>
+        <span className="block text-5xl text-white -mt-2">Repower Solutions</span>
       </Link>
       <HeaderNav />
     </header>
