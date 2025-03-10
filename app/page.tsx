573fd54b8cf79628dde8b4ae4b8d90c2f651bea8
Apostolos Karamanis <apostkaram@gmail.com>
2025-03-11 00:58:40 +0200

    removed duplicate aurora

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,6 @@ import Aurora from "@/components/aurora";
 export default function Home() {
   return (
     <div className="relative min-h-screen">
-      <div className="absolute top-0 left-0 right-0 h-20 z-10 overflow-hidden">
-        <div className="-mt-10">
-          {" "}
-          <Aurora
-            colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-            blend={0.5}
-            amplitude={1.0}
-            speed={0.5}
-          />
-        </div>
-      </div>
-
       {/* Your main content */}
       <div className="relative flex flex-col items-center justify-center min-h-screen py-2">
         <main className="z-10 text-center">
--- components/header/aurora-header.tsx
+++ components/header/aurora-header.tsx
@@ -6,7 +6,7 @@ export default function AuroraHeader() {
   return (
     <div className="fixed top-0 left-0 right-0 h-60 z-0 overflow-hidden">
       <Aurora
-        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+        colorStops={["#3E7B27", "#123524", "#3E7B27"]}
         blend={0.5}
         amplitude={1}
         speed={1}
